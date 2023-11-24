b0062f87e1cd76e02d63e995b9e723f5a5114a3e
ariefsusanto.id <ariefsusanto.id@gmail.com>
2023-11-25 00:41:29 +0700

    materi sentul

--- 3. Sources/membaca-qr-code/qrreader.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from pyzbar import pyzbar
-
-
-def read_qrcode_from_image(filename):
-    """Reads imagefile and returns list of decoded data"""
-    imgage = Image.open(filename)
-    datalist_from_image = pyzbar.decode(imgage)
-    return datalist_from_image
